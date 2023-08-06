7b8178084e516a7343ea4574c3d5ea977668ae43
giovaniMurakami <giovani.murakami@outlook.com>
2023-08-06 08:10:09 -0300

    added - export JSON

--- modules/bank_statements_utils.rb
+++ modules/bank_statements_utils.rb
@@ -17,7 +17,25 @@ module Bank_statements
         puts "============================="
     end
 
-    def self.export_statements()
+    def self.export_statements(account_id)
+        last_10_transactions = Transaction.where(account_id: account_id).order(Sequel.desc(:created_at)).limit(10)
+        transactions_array = []
 
+        last_10_transactions.each do |transaction|
+        transactions_array << {
+            transaction_id: transaction.id,
+            account_id: transaction.account_id,
+            amount: transaction.amount,
+            description: transaction.description,
+            created_at: transaction.created_at
+        }
+        end
+
+        uuid = UUID.new.generate
+        file_name = "last_10_transaction.json"
+
+        File.open(file_name, 'w') do |file|
+        file.puts JSON.pretty_generate(transactions_array)
+        end
     end
 end
